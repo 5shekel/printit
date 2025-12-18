f263ee7b304991d078a6710f0c24644b8f385757
morgulbrut <tillo.bosshart@gmail.com>
2025-12-18 12:43:46 +0100

    Updated faq.py

--- tabs/faq.py
+++ tabs/faq.py
@@ -9,11 +9,13 @@ def render():
     st.subheader("FAQ:")
     st.markdown(
         """
-        *dithering* is suggested (sometimes inforced) if source is not lineart as grayscale and color look bad at thermal printer
-
-        all uploaded images, generated labels, and webcam snapshots are saved
-
-        app [code](https://github.com/5shekel/printit)
+        - *dithering* is suggested (sometimes inforced) if source is not lineart as grayscale and color look bad at thermal printer
+        - switching tabs doesn't re-detect printers, refreshing the page and buttons will do it
+        - to save the generated images on the host, set `privacy_mode = false` in `config.toml`
+        - to disable printer sleep mode:
+            1. Discover the printer with `brother_ql discover`, it returns something like `Found compatible printer QL-600 at: usb://0x04f9:0x20c0/000H2G258173` were `usb://0x04f9:0x20c0/000H2G258173` is the printer id
+            2. Set the `power-off-delay` to 0: `brother_ql -p <PRINTER ID> configure set power-off-delay 0`. You can check the set value `brother_ql -p <PRINTER ID> configure get power-off-delay`
+        - [Repo](https://github.com/5shekel/printit)
 
         PRINT ALOT is the best!
         """
